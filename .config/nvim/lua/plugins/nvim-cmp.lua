995375c76f6b65b7acc39c78a97acd62e817bbe5
sigugo <tprell@gmail.com>
2021-09-07 16:40:14 +0200

    Quick Commit 2021-09-07.16:40:14

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,27 @@
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+        })
+},
+  sources = {
+      { name = 'nvim_lsp' },
+      { name = 'rust_analyzer' },
+      { name = 'vsnip' },
+      { name = 'path' },
+      { name = 'buffer' },
+  }
+})
